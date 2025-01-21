b34b2b5e901a3e175aad6645a0a3d42ea133c65e
VERNIOL Edgar <Edgar.Verniol@bordeaux-inp.fr>
2025-01-21 16:59:48 +0100

    lecture capteur i2C Co2 temp et Rh

--- main.cpp
+++ main.cpp
@@ -6,39 +6,39 @@
 #include "mbed.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "mbed_sensirion-scd4x/scd4x/scd4x.h"
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
-// Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
 
-using namespace std::chrono;
 
-Timer timer;
-InterruptIn bp(BUTTON1);
-microseconds freq = 100ms;
-Ticker flipper;
 
-void button(){
-    freq = freq + 50ms;
-}
+sixtron::SCD4X sensor(P1_I2C_SDA, P1_I2C_SCL);
+sixtron::SCD4X::ErrorType err;
 
-void notled(){
-    led = !led;
+//const int SCD4X_ADDR = 0x62 << 1; // 8bit I2C address
+
+static void display_value_result(sixtron::SCD4X::ErrorType err, uint16_t co2, double t, double rh)
+{
+    if (err != sixtron::SCD4X::ErrorType::Ok) {
+        printf("Error reading data: %d\n", err);
+    } else {
+        printf("CO2: %dppm, Temp: %f°C, RH: %f%%\n", co2, t, rh);
+    }
 }
 
 int main()
 {
-    bp.rise(&button);
-    //led = 1;
+    /* Force periodic measurement stop first and wait for its completion (500ms) */
+    sensor.start_periodic_measurement();
     while(1){
-        flipper.attach(&notled, freq);
+        sixtron::scd4x_measurement_t res;
+        sensor.read_measurement(&res);
+        display_value_result(err, res.co2, res.temperature, res.rh);
+
         ThisThread::sleep_for(BLINKING_RATE);
     }
+    sensor.stop_periodic_measurement();
 }
 
